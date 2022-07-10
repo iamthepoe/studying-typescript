c641ec9412a122ac795d6111b98fac16fcc1ddf7
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-07-10 12:25:28 -0300

    Create enum.ts

---
+++ src/BasicTypes/enum.ts
@@ -0,0 +1,18 @@
+enum Colors{
+	red, //0
+	blue, //1
+	green //2
+}
+
+enum Philosophies{
+	Platonism = 'p',
+	Aristotelism = 'a',
+	Randianism = 'r',
+	//Tomism, without initializer => 0, 1, 2 | r, ?, ?
+	Kantianism = 'k'
+}
+
+console.log(Colors.red === 0);
+console.log(Colors[0] === Colors[Colors.red]);
+console.log(Philosophies.Kantianism == Philosophies[Philosophies.Kantianism]);
+console.log(Philosophies[111]); //undefined 
